06abe9eaeb7d669bed3c7ea2e3878be43c04eb81
Ted Lilley <tlilley@diditbetter.com>
2014-01-16 16:45:04 -0500

    Allow removal of cookie domain for staging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SpreeDibs::Application.config.session_store :cookie_store, key: '_spree_dibs_session', domain: 'diditbetter.com'
+SpreeDibs::Application.config.session_store :cookie_store, key: '_spree_dibs_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
